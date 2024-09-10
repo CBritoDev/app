f41f9e9230a4f14c91d634e9c84d97844dc4cd6a
Carlos <cbritodeveloper@gmail.com>
2024-09-09 22:08:48 -0300

    Fundamentos 003

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
 // arrays, objetos array=vetor
 let meta = {       //{} após atribuição de valor significa que é um objeto que está sendo colocado
     value: 'ler um livro todo mês', 
-    checked: false,
-    log: (info) => {
-        console.log(info)
-    }
+    checked: true, //estilo variavel
 } 
-meta.value = "não é mais ler um livro"
-meta.log(meta.value)
+
+let metas = [
+    meta,
+    {
+        value: "caminhar 20 minutos todos os dias",
+        checked: false //na mao
+    }
+    
+]
+
+console.log(metas[1].value)
+
+//meta.value = "não é mais ler um livro"
+//meta.log(meta.value)
 
 //function // arrow function
 
-const criarMeta = () => {} //arrow function
+//const criarMeta = () => {} //arrow function
 
 //function criarMeta() {} //criacao de funcao
